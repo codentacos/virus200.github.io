1b53cac5b9dac28daa5f138305fa55a6fa704bca
Virus200 <nelsonz2013@hotmail.com>
2019-07-31 04:26:05 -0500

    new menu icon, flexbox update, toggle menu, js file added

---
+++ script.js
@@ -0,0 +1,7 @@
+let navbarToggle = document.getElementById('navbar-toggle-js');
+
+let navbar = document.getElementById('navbar-js')
+
+navbarToggle.addEventListener('click', function() {
+  navbar.classList.toggle('active');
+})
